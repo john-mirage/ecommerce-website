2273f0b5552ade6fcb9d705943b03fbef9e78ce3
john-mirage <chaosdynamix@posteo.net>
2022-07-31 04:30:56 +0200

    feat: add animation to the cart overview

--- src/components/app-cart-overview.js
+++ src/components/app-cart-overview.js
@@ -1,30 +1,30 @@
 import { cart } from "@utils/cart";
 
 const heartBeatAnimation = [
-  {
-    transform: "scale(1)",
-    offset: 0,
-  },
-  {
-    transform: "scale(1.5)",
-    offset: 0.14,
-  },
-  {
-    transform: "scale(1)",
-    offset: 0.28,
-  },
-  {
-    transform: "scale(1.5)",
-    offset: 0.42,
-  },
-  {
-    transform: "scale(1)",
-    offset: 0.7,
-  },
+  { transform: "scale(1)", offset: 0 },
+  { transform: "scale(1.5)", offset: 0.14 },
+  { transform: "scale(1)", offset: 0.28 },
+  { transform: "scale(1.5)", offset: 0.42 },
+  { transform: "scale(1)", offset: 0.7 },
 ];
 
 const heartBeatAnimationTiming = {
   duration: 1000,
+}
+
+const fadeInAndTranslateAnimation = [
+  { opacity: 0, transform: "translateY(-2rem)", offset: 0},
+  { opacity: 1, transform: "translateY(0)", offset: 1 }
+];
+
+const fadeOutAndTranslateAnimation = [
+  { opacity: 1, transform: "translateY(0)", offset: 0 },
+  { opacity: 0, transform: "translateY(-2rem)", offset: 1 }
+];
+
+const fadeAnimationTiming = {
+  duration: 300,
+  easing: "ease-in-out",
 }
 
 class AppCartOverview extends HTMLElement {
@@ -35,10 +35,46 @@ class AppCartOverview extends HTMLElement {
     this.modalElement = this.querySelector('[data-name="cart-modal"]');
     this.listElement = this.querySelector('[data-name="cart-list"]');
     this.cart = cart;
+    this.isOpen = false;
+    this.isAnimating = false;
+    this.handleButtonClick = this.handleButtonClick.bind(this);
   }
 
   connectedCallback() {
     this.updateBadge();
+    this.modalElement.style.display = "none";
+    this.buttonElement.addEventListener("click", this.handleButtonClick);
+  }
+
+  handleButtonClick() {
+    console.log(this.isAnimating);
+    if (!this.isAnimating) {
+      if (this.isOpen) {
+        this.closeModal();
+      } else {
+        this.openModal();
+      }
+    }
+  }
+
+  openModal() {
+    this.isAnimating = true;
+    this.modalElement.style.display = "block";
+    const fadeIn = this.modalElement.animate(fadeInAndTranslateAnimation, fadeAnimationTiming);
+    fadeIn.onfinish = () => {
+      this.isOpen = true;
+      this.isAnimating = false;
+    };
+  }
+
+  closeModal() {
+    this.isAnimating = true;
+    const fadeOut = this.modalElement.animate(fadeOutAndTranslateAnimation, fadeAnimationTiming);
+    fadeOut.onfinish = () => {
+      this.modalElement.style.display = "none";
+      this.isOpen = false;
+      this.isAnimating = false;
+    };
   }
 
   updateBadge() {
