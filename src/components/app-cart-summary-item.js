29deef6f80e632f75851d15bd75cde3106fe7a19
john-mirage <chaosdynamix@posteo.net>
2022-08-03 00:34:38 +0200

    feat: add cart summary items and abort controller

--- src/components/app-cart-summary-item.js
+++ src/components/app-cart-summary-item.js
@@ -1,3 +1,5 @@
+import { formatCameraPrice } from "@utils/camera-price-formatter";
+
 const template = document.getElementById("template-app-cart-summary-item");
 
 class AppCartSummaryItem extends HTMLElement {
@@ -5,6 +7,29 @@ class AppCartSummaryItem extends HTMLElement {
     super();
     this.initialCall = true;
     this.fragment = template.content.cloneNode(true);
+    this.imageElement = this.fragment.querySelector('[data-name="image"]');
+    this.nameElement = this.fragment.querySelector('[data-name="name"]');
+    this.priceElement = this.fragment.querySelector('[data-name="price"]');
+    this.numberElement = this.fragment.querySelector('[data-name="number"]');
+    this.lensElement = this.fragment.querySelector('[data-name="lens"]');
+  }
+
+  get item() {
+    const itemIsDefined = this.hasOwnProperty("_item");
+    if (itemIsDefined) {
+      return this._item;
+    } else {
+      throw new Error("The item is not defined");
+    }
+  }
+
+  set item(item) {
+    this._item = item;
+    this.imageElement.setAttribute("src", this.item.imageUrl);
+    this.nameElement.textContent = this.item.name;
+    this.priceElement.textContent = formatCameraPrice(this.item.price);
+    this.numberElement.textContent = String(this.item.number);
+    this.lensElement.textContent = String(this.item.lens);
   }
 
   connectedCallback() {
--- src/components/app-cart-summary.js
+++ src/components/app-cart-summary.js
@@ -1,4 +1,5 @@
 import { cart } from "@utils/cart";
+import { getOneCamera } from "@utils/camera-api";
 
 class AppCartSummary extends HTMLElement {
   constructor() {
@@ -8,6 +9,7 @@ class AppCartSummary extends HTMLElement {
     this.totalElement = this.querySelector('[data-name="total"]');
     this.appCartSummaryItem = document.createElement("app-cart-summary-item");
     this.appCartSummaryItemSkeleton = document.createElement("app-cart-summary-item-skeleton");
+    this.abortController = false;
   }
 
   connectedCallback() {
@@ -16,6 +18,44 @@ class AppCartSummary extends HTMLElement {
       const skeleton = this.appCartSummaryItemSkeleton.cloneNode(true);
       this.listElement.append(skeleton);
     }
+    this.displayCartItems();
+  }
+
+  disconnectedCallback() {
+    if (this.abortController) {
+      this.abortController.abort();
+      console.log("aborted");
+    }
+  }
+
+  async displayCartItems() {
+    let items = [];
+    uuidsLoop:
+    for (const uuid of this.cart.cart.keys()) {
+      this.abortController = new AbortController();
+      const signal = this.abortController.signal;
+      const { camera, error } = await getOneCamera(uuid, signal);
+      if (typeof error === "string") {
+        switch(error) {
+          case "aborted":
+            break uuidsLoop;
+          case "error":
+            break uuidsLoop;
+          case "not-found":
+            this.cart.deleteCameraByUuid(uuid);
+            break;
+          default:
+            throw new Error("unknown error");
+        }
+      } else {
+        const appCartSummaryItem = this.appCartSummaryItem.cloneNode(true);
+        appCartSummaryItem.item = camera;
+        items.push(appCartSummaryItem);
+      }
+    }
+    this.listElement.innerHTML = "";
+    this.listElement.append(...items);
+    this.abortController = false;
   }
 }
 
